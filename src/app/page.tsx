a4800837eb4345a23548cc7b80e84de29bf2bcd1
mmitusov <maksym.mitusov@gmail.com>
2024-02-09 10:05:54 +0200

    Front page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import * as Components from '@/components';
+const { About, Contact, Experience, Feedbacks, Hero, Loader, Navbar, Tech, Works } = Components;
+// import { About, Contact, Experience, Feedbacks, Hero, Loader, Navbar, Tech, Works } from '@/components'
+
+// import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Test
+  return ( 
+    <main className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
     </main>
   )
 }
