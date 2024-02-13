53fb9f447a629824ed4790c944f71b499b70707f
mmitusov <maksym.mitusov@gmail.com>
2024-02-13 09:42:20 +0200

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import { navLinks } from "@/config"
 import { logo, menu, close } from "../../public/media"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav>
-      Test
+    <nav className="paddingX w-full flex items-center p-5 fixed top-0 z-20 bg-primary">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        {/* <Link /> */}
+        Test
+      </div>
     </nav>
   )
 }
