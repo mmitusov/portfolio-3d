ba8d884f7fa1c3d55aa3d81fb117c849041b6e54
mmitusov <maksym.mitusov@gmail.com>
2024-02-14 21:18:42 +0200

    Img added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as Components from '@/components';
 const { About, Contact, Experience, Feedbacks, Hero, Loader, Navbar, Tech, Works } = Components;
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 
 const Navbar = () => {
   const [active, setActive] = useState('')
+  
   return (
     <nav className="paddingX w-full flex items-center p-5 fixed top-0 z-20 bg-primary">
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
@@ -13,6 +14,7 @@ const Navbar = () => {
           onClick={() => {setActive(''); window.scrollTo(0,0)}}
           className="flex items-center gap-2"
         >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
           Test
         </Link>
       </div>
