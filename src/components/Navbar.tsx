ec675125980ed027813d28497e9604a0d4f98620
mmitusov <maksym.mitusov@gmail.com>
2024-02-14 21:12:32 +0200

    Scroll on click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import { navLinks } from "@/config"
 import { logo, menu, close } from "../../public/media"
 import Link from "next/link"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
   return (
     <nav className="paddingX w-full flex items-center p-5 fixed top-0 z-20 bg-primary">
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
           href="/" 
-          onClick={() => }
+          onClick={() => {setActive(''); window.scrollTo(0,0)}}
           className="flex items-center gap-2"
         >
           Test
