8186e158a480c156e1d9de368464d5edb737019c
mmitusov <maksym.mitusov@gmail.com>
2024-02-27 10:46:00 +0200

    Hero section start

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,14 @@
+import { motion } from "framer-motion"
+import { ComputersCanvas } from "./canvas"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`flex flex-row items-start absolute inset-0 top-[120px] max-w-7xl mx-auto gap-5 paddingX`}>
+      Hero
+
+      </div>
+    </section>
   )
 }
 
