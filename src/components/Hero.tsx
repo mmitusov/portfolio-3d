a07578925abf48b9938791e9da268336eda4d563
mmitusov <maksym.mitusov@gmail.com>
2024-04-05 09:14:41 +0300

    Hero section update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,11 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`flex flex-row items-start absolute inset-0 top-[120px] max-w-7xl mx-auto gap-5 paddingX`}>
-        Hero
+        {/* Vertical line */}
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient"/>
+        </div>
       </div>
     </section>
   )
