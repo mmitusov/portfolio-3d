58667d6724740e54367302a642d944afe4ac95f1
mmitusov <maksym.mitusov@gmail.com>
2024-04-09 08:20:37 +0300

    Loader bug fix

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,11 @@
+//When you are using HTML inside the canvas, just wrap your HTML with Html helper from @react-three/drei
+import { Html } from "@react-three/drei"; 
+
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <Html>
+      <div>Loader</div>
+    </Html>
   )
 }
 
