bea3879fbb2e1272e929285ad0f72117842eb9af
mmitusov <maksym.mitusov@gmail.com>
2024-04-16 08:26:59 +0300

    Loader component update

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,33 @@
-//When you are using HTML inside the canvas, just wrap your HTML with Html helper from @react-three/drei
-import { Html } from "@react-three/drei"; 
+// When you are using HTML inside the canvas, just wrap your HTML with Html helper from @react-three/drei
+// useProgress чтобы отображать прогрсс подгрузки 3D модели в процентах
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => {
+  const { progress } = useProgress();
   return (
-    <Html>
-      <div>Loader</div>
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span> {/* Анимация крутилки */}
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%  {/* Это будет отображать прогрсс подгрузки 3D модели в процентах */}
+      </p>
     </Html>
-  )
-}
+  );
+};
 
-export default Loader
+export default CanvasLoader;
