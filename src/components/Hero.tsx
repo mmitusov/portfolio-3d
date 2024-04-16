961e9bb09fae7bd3b71cd41ca03da0d2a9defda4
mmitusov <maksym.mitusov@gmail.com>
2024-04-16 08:25:30 +0300

    Hero section update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion"
 import { ComputersCanvas } from "./canvas"
+import Link from "next/link"
 
 const Hero = () => {
   return (
@@ -21,6 +22,25 @@ const Hero = () => {
       </div>
 
         <ComputersCanvas />
+
+        {/* Button to scroll to the next section animated with the framer motion */}
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+          <Link href={"#about"}>
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+              <motion.div
+                animate={{
+                  y: [0, 24, 0], // We wan to animate only 'y' axis
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className='w-3 h-3 rounded-full bg-secondary mb-1' // Object that we want to animate
+              />
+            </div>
+          </Link>
+        </div>
     </section>
   )
 }
