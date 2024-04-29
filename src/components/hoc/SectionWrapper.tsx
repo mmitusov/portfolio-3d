6b0bfe001e882079edd9d4d8da625dee77dd8e86
mmitusov <maksym.mitusov@gmail.com>
2024-04-29 08:50:06 +0300

    SectionWrapper (HOC) added

---
+++ src/components/hoc/SectionWrapper.tsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion";
+import { staggerContainer } from "../../utils/motion";
+
+// HOC - это функция, что возвращает функцию, что возвращает компонент.
+const SectionWrapper = (Component: any, sectionId: string) => {
+  return function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: true, amount: 0.25 }}
+        className={`padding max-w-7xl mx-auto relative z-0`}
+      >
+
+        <Component />
+      </motion.section>
+    );
+  };
+};
+
+export default SectionWrapper;
