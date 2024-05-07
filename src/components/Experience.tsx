f89505ae1b095f345e377d9c1a3fa6e5f7b3fd18
mmitusov <maksym.mitusov@gmail.com>
2024-05-07 10:16:43 +0300

    Experience section update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,64 @@
-const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
-export default Experience
+import SectionWrapper from './hoc/SectionWrapper'
+import { experiences } from '@/config'
+import { textVariant } from '@/utils/motion'
+import useIsInView from "@/hooks/useIsInView";
+
+const Experience = () => {
+  const timelineElementRef = useRef(null);
+  const isInView = useIsInView(timelineElementRef);
+
+
+  return (
+    <div ref={timelineElementRef}>
+      <motion.div variants={textVariant(0.25)}>
+        <p className={`sectionSubText text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`sectionHeadText text-center`}>
+          Work Experience.
+        </h2>
+      </motion.div>
+
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+
+          {experiences.map((experience, index) => (
+            <VerticalTimelineElement
+              key={`experience-${index}`}
+              
+              visible={isInView}
+              contentStyle={{
+                background: "#1d1836",
+                color: "#fff",
+              }}
+              contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+              date={experience.date}
+              iconStyle={{ background: experience.iconBg }}
+              icon={
+                <div className='flex justify-center items-center w-full h-full'>
+                  <img
+                    src={experience.icon}
+                    alt={experience.company_name}
+                    className='w-[60%] h-[60%] object-contain'
+                  />
+                </div>
+              }
+            >
+            
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Experience, "work");
