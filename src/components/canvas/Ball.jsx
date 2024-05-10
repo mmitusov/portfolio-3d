d20f4616e63073693b76ca8fd5642ba864fbde18
mmitusov <maksym.mitusov@gmail.com>
2024-05-10 08:09:15 +0300

    Tech section update

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -10,8 +10,13 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+// Почему мы используем "icon.src", а не "icon"? Потому что "useTexture" принимает пареметром только путь к картике в виде строки
+// А Next.js по дефолту импортирует как объект. Поэтому если бы мы передали картику на прямую как объект, то у нас появилась бы ошибка 
+const Ball = ({icon}) => {
+  const [texture, textureError] = useTexture([icon.src]);
+  if (textureError) {
+    return <div>Error while loading texture!</div>;
+  }
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
@@ -29,7 +34,7 @@ const Ball = (props) => {
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
-          map={decal}
+          map={texture}
           flatShading
         />
       </mesh>
@@ -46,7 +51,7 @@ const BallCanvas = ({ icon }) => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
+        <Ball icon={icon} />
       </Suspense>
 
       <Preload all />
