70740ee5cb342063310ed6f2441777b311eb7a3b
mmitusov <maksym.mitusov@gmail.com>
2024-05-11 13:51:48 +0300

    Work section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import * as Components from '@/components';
-const { About, Contact, Experience, Feedbacks, Hero, Loader, Navbar, Tech, Works } = Components;
+const { About, Contact, Experience, Feedbacks, Hero, Loader, Navbar, Tech, Work } = Components;
 
 export default function Home() {
   return ( 
     <main className="relative z-0 bg-primary text-white">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> {/* We created 'bg-hero-pattern' inside tailwind.config.ts */}
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> {/* We created 'bg-hero-pattern' background inside tailwind.config.ts */}
         <Navbar />
         <Hero />
       </div>
@@ -13,7 +13,7 @@ export default function Home() {
       <About />
       <Experience />
       <Tech />
-      <Works />
+      <Work />
       <Feedbacks />
 
       <div className='relative z-0'>
