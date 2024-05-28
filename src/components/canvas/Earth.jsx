bc9e1422a68b76f4dad65b65d514d886f734c7a7
mmitusov <maksym.mitusov@gmail.com>
2024-05-28 08:43:02 +0300

    Earth canvas update

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -19,12 +19,6 @@ const EarthCanvas = () => {
       frameloop='demand'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
