67807632969f3ebe97ef477baaf3588470e85a47
mmitusov <maksym.mitusov@gmail.com>
2024-05-29 14:00:08 +0300

    framer-motion func bugfix

--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -61,7 +61,7 @@ export const slideIn = (direction: string, type: string, delay: number, duration
   return {
     hidden: {
       x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
-      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+      y: direction === "up" ? "-200%" : direction === "down" ? "200%" : 0,
     },
     show: {
       x: 0,
