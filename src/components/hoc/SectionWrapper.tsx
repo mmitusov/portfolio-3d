8d6b328200ceab8b97aef42e67aaa491951e1224
mmitusov <maksym.mitusov@gmail.com>
2024-05-30 12:41:49 +0300

    SectionWrapper description update

--- src/components/hoc/SectionWrapper.tsx
+++ src/components/hoc/SectionWrapper.tsx
@@ -4,6 +4,7 @@ import { staggerContainer } from "../../utils/motion";
 // HOC - это функция, что возвращает функцию, что возвращает компонент
 // React.ComponentType - типизация входящих Реакт компонентов
 // Этот HOC отвечает не только за стилизацию, но и за применение "framer-motion" параметров ко всем дочерним компонентам
+// "motion.section" можно заменить и на "motion.div", главное чтобы была обертка над другими "motion. " компонентами
 const SectionWrapper = (Component: React.ComponentType, sectionId: string) => {
   return function HOC() {
     return (
