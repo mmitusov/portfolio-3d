ba8f4aed1f1daf18624d14b18db99db27c729c1c
mmitusov <maksym.mitusov@gmail.com>
2024-08-31 13:23:12 +0300

    nextConfig updater for deploying to GitHub pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   //Here is "Next config" if you want to display any images in nextjs app from accross the internet
   images: {
     remotePatterns: [
