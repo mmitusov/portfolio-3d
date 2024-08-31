e068b824f2fbb3efadbf65899f5b427bda747af5
mmitusov <maksym.mitusov@gmail.com>
2024-08-31 13:51:44 +0300

    Updated path to work with GitHub pages

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   output: 'export',
   //Here is "Next config" if you want to display any images in nextjs app from accross the internet
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
