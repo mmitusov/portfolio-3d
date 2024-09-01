b25d9a1c5af142861450b9a5cd8cd5b9850d9935
mmitusov <maksym.mitusov@gmail.com>
2024-09-01 13:47:41 +0300

    GitHub pages bugfix test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/mitusov-portfolio-3d",
   output: 'export',
   //Here is "Next config" if you want to display any images in nextjs app from accross the internet
   images: {
