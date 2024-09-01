afde84cecaf4f6c8195bdec28fe7c49b93a880f2
mmitusov <maksym.mitusov@gmail.com>
2024-09-01 14:45:32 +0300

    GitHub pages bugfix test

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("/planet/scene.gltf");
+  // const earth = useGLTF("/planet/scene.gltf");
+  const earth = useGLTF("/mitusov-portfolio-3d/planet/scene.gltf");
   // const earth = useGLTF(`${process.env.NEXT_PUBLIC_BASE_PATH}/planet/scene.gltf`);
   console.log("earth", earth)
 
