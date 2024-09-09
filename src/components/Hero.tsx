1bdeba2f59a8a2770e935e1f59aa214e2d1d1b4b
mmitusov <maksym.mitusov@gmail.com>
2024-09-09 08:15:14 +0300

    Hero section - stretched canvas fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,26 +22,29 @@ const Hero = () => {
         </div>
       </div>
 
+      {/* Add wrapper for the 3D Canvas to position it correctly */}
+      <div className="absolute bottom-20 w-full h-[350px] sm:h-[600px] sm:bottom-0">
         <ComputersCanvas />
+      </div>
 
-        {/* Button to scroll to the next section animated with the framer motion */}
-        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-          <Link href={"#about"}>
-            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-              <motion.div
-                animate={{
-                  y: [0, 24, 0], // We wan to animate only 'y' axis
-                }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: "loop",
-                }}
-                className='w-3 h-3 rounded-full bg-secondary mb-1' // Object that we want to animate
-              />
-            </div>
-          </Link>
-        </div>
+      {/* Button to scroll to the next section animated with the framer motion */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <Link href={"#about"}>
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0], // We wan to animate only 'y' axis
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1' // Object that we want to animate
+            />
+          </div>
+        </Link>
+      </div>
     </section>
   )
 }
