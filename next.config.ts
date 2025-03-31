226272173d946402524746ab457a5a3611c2ae4d
Maksym Mitusov <maksym.mitusov@gmail.com>
2025-03-31 11:52:23 +0300

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // generating HTML files that can be deployed to any static hosting platform (e.g., Vercel, Netlify, GitHub Pages).
+  output: 'export', // used to export the website as a fully static site, 
+
   //Here is "Next config" if you want to display any images in nextjs app from accross the internet
   images: {
     unoptimized: true,
@@ -19,6 +22,6 @@ const nextConfig = {
       },
     ],
   }
-}
+};
 
 export default nextConfig;
