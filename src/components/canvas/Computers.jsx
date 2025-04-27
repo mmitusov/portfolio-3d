6060066efaab71a0d866eddcab2d9c69117cbb04
Maksym Mitusov <maksym.mitusov@gmail.com>
2025-04-27 10:21:04 +0300

    Computer model update

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,7 @@
-import { Suspense, useEffect, useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber"; //Просто пустой канвас в который мы можем что-то поместить
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber"; //Просто пустой канвас в который мы можем что-то поместить
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei"; //Вспомогательные инструменты которыми мы будем "рисовать" на пустом канвасе. "useGLTF" помогает ипортировать 3D модели
+import { useComputerScale } from "../../hooks/useModelScale";
 
 // We can not use HTML tags inside the canvas. To solve this, we wraped the CanvasLoader inside "import { Html } from "@react-three/drei";"
 import CanvasLoader from "../Loader";
@@ -9,6 +10,7 @@ const Computers = ({ isMobile }) => {
   // Не забываем, что абсолютный путь в Next.js автоматически начинается с папки 'public'. Поэтому путь к файлу выглядет следующим образом
   const basePath = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_GIT_HUB_PAGES_BASE_PATH : '';
   const computer = useGLTF(`${basePath}/desktop_pc/scene.gltf`);
+  const { computerScale } = useComputerScale();
   
   // Работая с ThreeJS вместо <div> мы используем <mesh>. Внутри <mesh> важно создать освещение, иначе мы ничего не будем видеть
   // Чтобы отобразить модельку, далее мы должны поместить ее внутри Canvas. Не обязательно, но создадим ниже отдельный компонентик для этого
@@ -26,7 +28,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.6 : 0.75}
+        scale={computerScale}
         position={isMobile ? [0, -1.5, -1.9] : [0, -1.55, -1.1]}
         rotation={[0, 0, 0]} // To change angle of how model facing us
       />
@@ -38,25 +40,7 @@ const Computers = ({ isMobile }) => {
 // Быстрее и удобнее было бы подгружать 3D модельки используя SSR, но по приколу потренеруемся с Suspense
 // OrbitControls - помогает крутить нашу модельку (делает ее интерактивной)
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // During the first page load check if it's a mobile devise
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-    // Add a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
+  const { isMobile } = useComputerScale();
 
   return (
     <Canvas
