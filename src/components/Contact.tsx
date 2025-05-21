528eb9bfaa544bce23f142f00cd56c80862bd664
Maksym Mitusov <maksym.mitusov@gmail.com>
2025-05-21 19:34:41 +0300

    Contact update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -118,9 +118,11 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px] relative w-full'
       >
-        <EarthCanvas />
+        <div className='absolute inset-0 w-full h-full'>
+          <EarthCanvas />
+        </div>
       </motion.div>
     </div>
   )
